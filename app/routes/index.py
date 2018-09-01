641b6c80b7cfe9fcb6d00ce102765cd47e8335f0
Marek Ventur <marekventur@gmail.com>
2018-09-01 16:42:57 +0100

    Add poke endpoint

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify, Blueprint
 from ..flask_shared import app
+from ..util.memcached import shared
+import shutil
+from os.path import *
+import tempfile
 
 index_routes = Blueprint('index_routes', __name__)
 
@@ -7,3 +11,12 @@ index_routes = Blueprint('index_routes', __name__)
 def hello_world():
     app.logger.info("Hello world")
     return jsonify({"hello": "world"})
+
+@index_routes.route("/poke")
+def poke():
+    app.logger.info("Flush")
+    shared.flush_all()
+    app.logger.info("Remove")
+    shutil.rmtree(normpath(tempfile.gettempdir() + "/badgestore-cache/"))
+    app.logger.info("Done")
+    return jsonify({"poked": True})
